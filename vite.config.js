e0fd5dbb1da0e57a3e29251774eb0fb75796b372
Shorya Mittal <shoryamittal12345@gmail.com>
2026-09-04 17:44:38 +0530

    fix: set base to relative for universal hosting on GitHub Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',
   },
